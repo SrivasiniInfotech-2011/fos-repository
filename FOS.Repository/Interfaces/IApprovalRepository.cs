060c7ac2a8b79e32f76a18942a830a46b2f4ae52
srivatsan1982 <seshadri.srivatsan@gmail.com>
2025-02-01 12:03:50 +0530

    Added New Feature Branch for BM Approval

---
+++ FOS.Repository/Interfaces/IApprovalRepository.cs
@@ -0,0 +1,16 @@
+﻿using FOS.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOS.Repository.Interfaces
+{
+    public interface IApprovalRepository
+    {
+        public Task<IEnumerable<Lookup>> GetLookupsForApproval();
+        public Task<IEnumerable<Lookup>> GetLookupsForApproval();
+
+    }
+}
